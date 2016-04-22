82dda29372f2cb0375e8a302c4fb7687fd5983ed
Matthew Cunliffe <matthew.cunliffe12@imperial.ac.uk>
2016-04-22 14:08:00 +0100

    Created root menu + added label to strings.xml

---
+++ app/src/main/java/com/example/matthew/mapdirections/RootMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.matthew.mapdirections;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RootMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_root_menu);
+    }
+}
