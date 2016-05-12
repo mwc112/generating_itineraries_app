b10de1bfa60209baa73b6bf2c94e1c6bc3e99798
Matthew Cunliffe <matthew.cunliffe12@imperial.ac.uk>
2016-05-12 16:18:16 +0100

    Added activity when at location

---
+++ app/src/main/java/com/example/matthew/mapdirections/RunningTripAtLocActivity.java
@@ -0,0 +1,53 @@
+package com.example.matthew.mapdirections;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RunningTripAtLocActivity extends AppCompatActivity {
+
+    private ServiceConnection connection;
+    private testService boundService;
+    private boolean isBound = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_running_trip_at_loc);
+
+        connection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                boundService = ((testService.TestBinder)service).getService();
+                boundService.setRunningTripAtLocActivity(RunningTripAtLocActivity.this);
+                boundService.setIsShowing(true);
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+
+            }
+        };
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(isBound) {
+            boundService.repushNotification();
+            boundService.setIsShowing(false);
+            unbindService(connection);
+            isBound = false;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        bindService(new Intent(this, testService.class), connection, BIND_AUTO_CREATE);
+        isBound = true;
+    }
+}
